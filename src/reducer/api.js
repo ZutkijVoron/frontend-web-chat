b1fd0ef7f62f006c7583bd5747166d78eb971d85
ZutkijVoron <zutkijvoron44@gmail.com>
2025-04-16 13:44:29 +0500

    fix env variable

--- src/reducer/api.js
+++ src/reducer/api.js
@@ -2,12 +2,16 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     register: process.env.REGISTER
+        || process.env.REACT_APP_REGISTER
         || 'http://192.168.1.212:5000/auth/register',
     login: process.env.LOGIN
+        || process.env.REACT_APP_LOGIN
         || 'http://192.168.1.212:5000/auth/login',
     connect: process.env.CONNECT
+        || process.env.REACT_APP_CONNECT
         || 'http://192.168.1.212:5000/chat',
     sendMessage: process.env.SENDMESSAGE
+        || process.env.REACT_APP_SENDMESSAGE
         || 'http://192.168.1.212:5000/chat'
 }
 
